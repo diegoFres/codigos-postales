41b73fa8dabfe8739c56687c58f77cf36ce5c58b
diegofres <a223221580@unison.mx>
2024-08-14 14:41:02 -0700

    hola

--- src/main/java/mx/unison/Main.java
+++ src/main/java/mx/unison/Main.java
@@ -1,12 +1,34 @@
 package mx.unison;
-
-/* Contar cuantos códigos postales corresponden a asentamientos rurales
-y cantos a asentamientos rurales
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        String archivo = "C:\\Users\\diego\\IdeaProjects\\codigospostales\\codigos_postales.csv";
+        String linea;
+        String sep = ",";
 
-        System.out.println("Hello world!");
+        int Urbanos = 0;
+        int Rurales = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(sep);
+
+
+                String tipoAsent = datos[2].trim();
+
+                if (tipoAsent.equalsIgnoreCase("Urbano")) {
+                    Urbanos++;
+                } else if (tipoAsent.equalsIgnoreCase("Rural")) {
+                    Rurales++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Asentamientos Urbanos: " + Urbanos);
+        System.out.println("Asentamientos Rurales: " + Rurales);
     }
 }
