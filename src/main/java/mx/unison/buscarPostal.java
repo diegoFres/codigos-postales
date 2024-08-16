b2dafc7aaf041505b9a28881075354114702d78d
diegofres <a223221580@unison.mx>
2024-08-15 18:28:45 -0700

    commit 2

--- src/main/java/mx/unison/buscarPostal.java
+++ src/main/java/mx/unison/buscarPostal.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 public class buscarPostal {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Por favor, proporciona al menos un código postal como argumento.");
-            return;
-        }
 
         String archivo = "C:\\Users\\diego\\IdeaProjects\\codigospostales\\codigos_postales.csv";
         String linea;
         String separador = ",";
+
+     catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
